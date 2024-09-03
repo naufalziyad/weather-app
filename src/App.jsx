f667a67c862f2d3f7ce1e003d8208b4682d3f5c3
naufalziyad <naufal.polinema@gmail.com>
2024-09-03 18:04:40 +0700

    upd: formatter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import Weather from './components/Weather'
+import React from "react";
+import Weather from "./components/Weather";
 
 const App = () => {
   return (
-    <div className='app'>
+    <div className="app">
       <Weather />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
